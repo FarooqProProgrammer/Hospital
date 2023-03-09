4cd6e08523e1670284d255aa6959ef5ce0d7d1d2
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-03-09 23:05:25 +0500

    Updated

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Slider from "react-slick";
 
 // ============= Images ================
 import PriceOne from "../../assets/price-1.jpg"
@@ -8,19 +7,69 @@ import PriceTwo from "../../assets/price-2.jpg"
 import PriceThree from "../../assets/price-3.jpg"
 import PriceFour from "../../assets/price-4.jpg"
 
-import Carousel from 'react-bootstrap/Carousel';
 
 
 export default function Pricing() {
     return (
-        <div class="container-fluid py-5">
-            <div class="container">
-                <div class="text-center mx-auto mb-5" style={{ maxWidth: "500px" }}>
-                    <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5">Medical Packages</h5>
-                    <h1 class="display-4">Awesome Medical Programs</h1>
+      
+        <div className="bg-gray-100 my-5">
+                <div className="max-w-7xl m-auto">
+                    <section className='my-24'>
+                        <header className="text-center">
+                            <h3 className="text-5xl font-bold mb-5 text-gray-900">
+                                Our Pricing Plan
+                            </h3>
+                            <p className="text-xl text-gray-400">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, natus?
+                            </p>
+                            <p className="text-xl font-bold text-gray-500">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, natus?
+                            </p>
+                            <div className="flex flex-col mt-10 mx-5 space-y-5 md:space-y-0 md:flex-row md:space-x-5 justify-between">
+                                <div className="flex-1">
+                                    <div className="bg-white p-10 rounded-lg">
+                                        <div className="flex justify-between items-center">
+                                            <div className="">
+                                                <h4 className="text-2xl font-bold text-gray-900">
+                                                    Basic
+                                                </h4>
+                                                <p className="text-xs text-gray-500">Most Popular</p>
+                                            </div>
+                                        </div>
+                                        <div className="bg-gray-100 p-2 rounded-lg text-center">
+                                            <h4 className="text-2xl font-bold text-gray-900">Basic</h4>
+                                            <p className="text-xs text-gray-500">Per Month</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex-1">
+                                    <div className="bg-white p-10 rounded-lg">
+                                        <div className="flex justify-between items-center">
+                                            <div className="">
+                                                <h4 className="text-2xl font-bold text-gray-900">
+                                                    Basic
+                                                </h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex-1">
+                                    <div className="bg-white p-10 rounded-lg">
+                                        <div className="flex justify-between items-center">
+                                            <div className="">
+                                                <h4 className="text-2xl font-bold text-gray-900">
+                                                    Basic
+                                                </h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </header>
+                    </section>
                 </div>
-                    
-            </div>
         </div>
+      
     )
 }
